1e1cecf7e2243d2d6ddec4c1d7141d33d09b352d
Bloodiey <bloodmoondesignx86@gmail.com>
2024-01-10 11:42:12 -0600

    Agregar archivos de proyecto.

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ovin24PCE
+{
+    public class program
+    {
+        static public void Main(string[] args) {
+
+            Console.WriteLine("Starting Up");
+
+            // TODO: init engine
+        
+        }
+    }
+}
